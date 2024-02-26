ea37d89f0329caefaf124290884c93056b28ea3e
letssrockit <leticiarocha_22@outlook.com.br>
2024-02-26 15:06:29 -0300

    Criação dos componentes temas e postagens

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ import Cadastro from './paginas/cadastro/Cadastro';
 import Home from './paginas/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
 import ListaTemas from './components/temas/listaTemas/ListaTemas';
+import FormularioTema from './components/temas/formularioTema/FormularioTema';
+import FormularioPostagem from './components/formularioPostagem/FormularioPostagem';
+import ListaPostagens from './components/postagens/listaPostagens/ListaPostagens';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import DeletarPostagem from './components/deletarPostagem/DeletarPostagem';
 
 
 function App() {
@@ -23,6 +28,13 @@ function App() {
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
               <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+              <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
             </Routes>
           </div>
           <Footer />
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://https://blogpessoal-22ho.onrender.com/'
+  baseURL: 'https://blogpessoal-22ho.onrender.com/'
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
